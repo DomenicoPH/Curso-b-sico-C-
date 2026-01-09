1177a9e29ff75b2bbee579405fcc2fb98e4c4622
Domenico Pagano <dpaganoh@gmail.com>
2026-01-08 22:33:41 -0500

    lectura y escritura de datos

--- CursoBasico/ProgramClase07.cs
+++ CursoBasico/ProgramClase07.cs
@@ -6,7 +6,30 @@ namespace CursoBasico
     {
         public static void Ejecutar()
         {
-            
+
+            // Lectura de datos
+
+            // Solicita nombre:
+
+            string nombre = "";
+
+            Console.WriteLine("Ingresa tu nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("\n Tu nombre es: " + nombre);
+
+
+            Console.WriteLine("-------");
+
+            // Suma números
+            Console.WriteLine("Ingresa el primer número");
+            int num1 = Convert.ToInt32( Console.ReadLine() );
+
+            Console.WriteLine("Ingresa el segundo número");
+            int num2 = Convert.ToInt32( Console.ReadLine() );
+
+            int resultadoSuma = num1 + num2;
+            Console.WriteLine("la suma de los dos números es: " + resultadoSuma);
 
 
         }
